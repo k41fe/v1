2de0dee2093290a6ad4d0013d37d51aa1f76115e
TTTTZ9 <112633409+TTTTZ9@users.noreply.github.com>
2022-09-03 02:57:34 +0300

    Update inline.py

--- YukkiMusic/plugins/bot/inline.py
+++ YukkiMusic/plugins/bot/inline.py
@@ -54,17 +54,17 @@ async def inline_query_handler(client, query):
                 ]
             )
             searched_text = f"""
-❇️**Title:** [{title}]({link})
+**Title:** [{title}]({link})
 
-⏳**Duration:** {duration} Mins
-👀**Views:** `{views}`
-⏰**Published Time:** {published}
-🎥**Channel Name:** {channel}
-📎**Channel Link:** [Visit From Here]({channellink})
+**Duration:** {duration} Mins
+**Views:** `{views}`
+**Published Time:** {published}
+**Channel Name:** {channel}
+**Channel Link:** [Visit From Here]({channellink})
 
 __Reply with /play on this searched message to stream it on voice chat.__
 
-⚡️ ** Inline Search By {MUSIC_BOT_NAME} **"""
+ ** Inline Search By {MUSIC_BOT_NAME} **"""
             answers.append(
                 InlineQueryResultPhoto(
                     photo_url=thumbnail,
