2c3c72cc587133fa6361d69a2436f7a430b33cec
TTTTZ9 <112633409+TTTTZ9@users.noreply.github.com>
2022-09-03 03:15:20 +0300

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -57,8 +57,8 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text=_["SOURCE_DRAGON"],
-                url=f"https://t.me/yy8gg",
+                text=_["SOURCE_NELOVER"],
+                url=f"https://t.me/EI222",
             ),
         ],
     ]
